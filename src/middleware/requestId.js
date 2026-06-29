d24c1d7f54276f3277e43df2cb4151d7e835eb3d
Crakcker-Coklat <cracker.coklat@gmail.com>
2026-06-29 16:14:47 +0700

    Revert "first up"
    This reverts commit 12bd616eaf99d5a8ad85b7a4e8651cbd360631ed.

--- src/middleware/requestId.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-function requestId(req, _res, next) {
-  req.id = `req-${Date.now().toString(36)}-${crypto.randomUUID().slice(0, 8)}`;
-  next();
-}
-
-module.exports = requestId;
--- src/services/cache.js
+++
@@ -1,9 +0,0 @@
-const NodeCache = require('node-cache');
-const config = require('../config');
-
-const cache = new NodeCache({
-  stdTTL: config.cache.ttl,
-  checkperiod: 60,
-});
-
-module.exports = cache;
